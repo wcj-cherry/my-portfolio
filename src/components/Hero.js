4ae961de59b799223156b6e85273bb883b520eeb
wcj-cherry <cherrythejerry@gmail.com>
2025-06-30 00:49:59 +0530

    first build fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
       <div className="h-screen flex flex-col justify-center items-center text-center 
     bg-gradient-to-r from-purple-500 to-indigo-600 text-white">
-      <h1 className="text-5xl font-bold">Hi, I'm Sujit 👋</h1>
+      <h1 className="text-5xl font-bold">Hi, I&apos;m Sujit 👋</h1>
       <p className="mt-4 text-xl">Java Developer | React Enthusiast | Spring Boot Pro</p>
       <a
         href="/contact"
